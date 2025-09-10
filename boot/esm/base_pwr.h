74de480a06c33f2edfb948381b51731a1a4320f2
Jowllel <joel.schaller16@gmail.com>
2025-09-10 16:11:26 +0200

    Entering Low Power Mode, when lowpower bit in nvs is detected.

---
+++ boot/esm/base_pwr.h
@@ -0,0 +1,17 @@
+/****************************************************
+* @file         base_pwr.h
+ * @author      jschaller
+ * @date        07.11.2024
+ * @version     1.0
+ * @brief       Module which provides functions to init and control power lines for esm base.
+ ***************************************************/
+
+#ifndef BASE_PWR_H
+#define BASE_PWR_H
+
+#include "zephyr/kernel.h"
+
+void base_pwr_init(void);
+void base_pwr_set(int id, bool state);
+
+#endif //BASE_PWR_H
--- boot/esm/esm.c
+++ boot/esm/esm.c
@@ -11,8 +11,28 @@
 #include "bootutil/bootutil_log.h"
 
 #include "nvs.h"
+#include "base_pwr.h"
 
 BOOT_LOG_MODULE_REGISTER(esm);
+
+
+void HAL_PWR_EnterSTANDBYMode(void)
+{
+ /* Select STANDBY mode */
+ SET_BIT(PWR->PMCR, PWR_PMCR_LPMS);
+
+ /* Set SLEEPDEEP bit of Cortex System Control Register */
+ SET_BIT(SCB->SCR, ((uint32_t)SCB_SCR_SLEEPDEEP_Msk));
+
+ /* Wait For all memory accesses to complete before continuing */
+ __DSB();
+
+ /* Ensure that the processor pipeline is flushed */
+ __ISB();
+
+ /* Wait For Interrupt Request */
+ __WFI();
+}
 
 void esm_boot_routine()
 {
@@ -22,5 +42,15 @@ void esm_boot_routine()
 	{
 		LOG_INF("Entering Low Power Mode !!!!");
 		util_nvs_low_power_state_set(LOW_POWER_STATE_OFF);
+		
+		base_pwr_init();
+
+		base_pwr_set(1, false);
+		base_pwr_set(2, false);
+		base_pwr_set(3, false);
+		
+		base_pwr_set(0, false);
+		
+		HAL_PWR_EnterSTANDBYMode();
 	}
 }
