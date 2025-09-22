094670e341015628f8ccd14080d4a4a4e70a3560
Jowllel <joel.schaller16@gmail.com>
2025-09-22 13:08:41 +0200

    Moved the code to enter the low power mode to the main firmware. Only enabling peripheral supplies in bootloader.

--- boot/esm/esm.c
+++ boot/esm/esm.c
@@ -10,40 +10,14 @@
 
 #include "bootutil/bootutil_log.h"
 
-#include "nvs.h"
 #include "base_pwr.h"
+
 
 BOOT_LOG_MODULE_REGISTER(esm);
 
 
 void esm_boot_routine()
 {
-	util_nvs_init();
 	base_pwr_init();
-
-	switch(util_nvs_low_power_state_get())
-	{
-		case LOW_POWER_STATE_TRIG:
-			LOG_INF("Entering Low Power Mode !!!!");
-			util_nvs_low_power_state_set(LOW_POWER_STATE_ON);
-
-			base_pwr_set(1, false);
-			base_pwr_set(2, false);
-			base_pwr_set(3, false);
-			
-			base_pwr_set(0, false);
-			
-			//Enter Standby Mode
-			SET_BIT(PWR->PMCR, PWR_PMCR_LPMS);
-			SET_BIT(SCB->SCR, ((uint32_t)SCB_SCR_SLEEPDEEP_Msk));
-			__DSB();
-			__ISB();
-			__WFI();
-			
-			break;
-			
-		case LOW_POWER_STATE_ON:
-			base_pwr_set(0, true);
-			break;
-	}
+	base_pwr_set(0, true);
 }
